64abfad8a7ba7cbf8c43b0ebda6752f487b5ee58
Vivek Patel <43326066+vivek1999patel@users.noreply.github.com>
2025-10-24 19:29:26 -0400

    Added Data Transformation Pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.textSummarizer.pipeline.data_transformation_pipeline import DataTransformationPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -7,6 +8,17 @@ try:
     logger.info(f"Stage {STAGE_NAME} initiated")
     data_ingestion_pipeline=DataIngestionPipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Data Transformation Stage"
+
+try:
+    logger.info(f"Stage {STAGE_NAME} initiated")
+    data_transformation_pipeline=DataTransformationPipeline()
+    data_transformation_pipeline.initiate_data_transformation()
     logger.info(f"Stage {STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
